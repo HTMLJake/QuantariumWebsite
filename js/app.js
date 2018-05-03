987e8c8810c8d2cd55f847f87f01a6dcc9772b36
Jacob Perry <btjake@TheMetalFruit.home>
2018-05-03 00:30:18 -0700

    Init commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/**
+ * @description Navbar element
+ */
+var navElement = document.getElementById("nav");
+
+// Adjust nav style when scrolling to hide or show depending on Scroll Y value
+window.addEventListener("scroll", e => {
+    let navY = navElement.style.top.slice(0, -2);
+    if(scrollY < (5 * 15)) {
+        navElement.style.top = `${15 - (scrollY / 5)}px`;
+        navElement.style.width = `${85 + (scrollY / 5)}%`
+        navElement.style.backgroundColor = `rgba(48, 48, 48, ${scrollY / 75})`;
+    } else {
+        //Once passed just set to expected values to bypass refresh rate issues
+        navElement.style.top = "0px";
+        navElement.style.width = "98vw";
+        navElement.style.backgroundColor = `rgba(48, 48, 48, 1)`;
+    }
+});
+
+/** 
+**  @description Get element's Y position and scroll to that location with Scroll y and nav height offset 
+**  @param {DOMElement} section DOMElement of the section to scroll to    
+**/
+function scrollToLoc(section) {
+    let sectionElement = document.getElementById(section);
+    if(sectionElement) {
+        console.log(sectionElement.getBoundingClientRect().y);
+        window.scrollTo({
+            top: sectionElement.getBoundingClientRect().y + scrollY - 75,
+            behavior: "smooth"
+        })
+    }
+}
+
+/**
+ * @description Scrolls to the top of the page
+ */
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
---
+++ js/sidebar.js
@@ -0,0 +1,34 @@
+/* 
+*   TODO: Don't toggle off when selecting currently selected
+*   TODO: Show Home selected on load
+*/
+
+/** 
+**  @description Toggles sidebar visibilty  
+**  @param {DOMElement} Hamburger Hamburger Element reference    
+**/
+function toggleSidebar(hamburger) {
+    var sidebarElem = window.document.getElementById("sidebar");
+    var mainElem = window.document.getElementsByClassName("main")[0];
+    sidebarElem.classList.toggle("open");
+    hamburger.classList.toggle("open")
+}
+
+var prevSelection;
+
+function SidebarSelection(item) {
+    if(prevSelection) {
+        if(prevSelection === item) {
+            item.classList.toggle("selected");
+            prevSelection = undefined;
+        } else {
+            prevSelection.classList.toggle("selected");
+            item.classList.toggle("selected");
+            prevSelection = item;
+        }
+    } else {
+        item.classList.toggle("selected");
+        prevSelection = item;
+        toggleSidebar()
+    }
+}
